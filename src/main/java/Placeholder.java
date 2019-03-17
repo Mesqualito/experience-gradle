40330ef90a5bd0d4c998eee9126d60f4d541d7f5
Mesqualito <jochen@gebsattel.rocks>
2019-03-17 16:44:11 +0100

    I don't wnat to deal with separate language-specific source files now. I only use gradle at the moment :-)
    Closes #7

--- src/main/java/Placeholder.java
+++
@@ -1,6 +0,0 @@
-public class Placeholder {
-
-        String comment=
-                "SourceSets declare, where gradle with its \'java\'-plugin will look into." +
-                "They will not be rolled out automatically except by Intellij or other IDE.";
-}
