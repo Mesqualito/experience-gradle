4f743d12324028021185290e2a0bc658fb7878e3
Mesqualito <jochen@gebsattel.rocks>
2019-03-17 16:38:48 +0100

    SourceSets declare, where gradle with its 'java'-plugin will look into. They will not be rolled out automatically except by Intellij or other IDE!
    Next thing to understand:
    https://docs.gradle.org/current/userguide/organizing_gradle_projects.html#sec:separate_test_type_source_files
    - Why in 'gradle' sub-directory?
    - How to link to the 'gradle/integration-test.gradle'?
    - Has the plugin 'java' to be declared twice then?
    
    Relates to #7

---
+++ src/main/java/Placeholder.java
@@ -0,0 +1,6 @@
+public class Placeholder {
+
+        String comment=
+                "SourceSets declare, where gradle with its \'java\'-plugin will look into." +
+                "They will not be rolled out automatically except by Intellij or other IDE.";
+}
